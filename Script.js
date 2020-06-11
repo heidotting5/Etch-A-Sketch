2df667bc52817942d84cd28aca1f183440973385
Jared Heidotting <jheidotting@gmail.com>
2020-06-11 07:55:46 -0400

    Added ability for users to choose grid size and color

--- Script.js
+++ Script.js
@@ -1,18 +1,15 @@
-
-// Calling main function
-createGrid();
-
-
 // Creates an Etch-A-Sketch grid        
-function createGrid () {
+document.getElementById('CTA-Button').onclick = function () {
     var gridSize = getUserInput();
+    var gridColor = document.querySelector("#colors");
+    var color = gridColor.options[gridColor.selectedIndex].text;
 
     for (x=1; x <= gridSize; x++) {
         for (y=1; y <= gridSize; y++) {
             const grid = document.querySelector('.main-container');
             
             const newSquare = document.createElement('div');
-            newSquare.style.cssText = 'background-color: black; margin: 0.5px; opacity: 0.1;'
+            newSquare.style.cssText = 'background-color: '+color+'; margin: 0.5px; opacity: 0.1;'
             newSquare.classList.add('block');
 
             grid.append(newSquare);
@@ -31,14 +28,14 @@ function createGrid () {
 
 // Uses user input to set grid size and arrange elements on the screen
 function getUserInput () {
-    var input = prompt("Please enter a grid size");
+    var input = document.getElementById('grid-size').value;
     var container = document.querySelector('.main-container')
     var width;
     
     if(input > 0 && input <= 15) {
-      container.style.gridTemplateColumns = 'repeat('+input+', 50px)'
-      container.style.gridTemplateRows = 'repeat('+input+', 50px)'
-      width = 50*input; 
+      container.style.gridTemplateColumns = 'repeat('+input+', 60px)'
+      container.style.gridTemplateRows = 'repeat('+input+', 60px)'
+      width = 60*input; 
 
     } else if (input > 15 && input <= 30) {
       container.style.gridTemplateColumns = 'repeat('+input+', 30px)'
@@ -56,4 +53,3 @@ function getUserInput () {
 
     return input;
 }
-
